cf9c6a0bf71a9dced8376729c28b694b00f63fa1
rishab2890 <rishabsharma2890@gmail.com>
2020-08-31 16:07:12 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:digibit_chat/welcomescreen.dart';
+import 'package:flutter/material.dart';
+import 'welcomescreen.dart';
+
+void main() => runApp(DigiBit());
+
+class DigiBit extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: WelcomeScreen(),
+    );
+  }
+}
